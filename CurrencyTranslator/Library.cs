4d24acec0db386ba7724ac7c618299efae2cadf4
JayCLFX <JayClfx@gmail.com>
2023-11-16 13:17:52 +0100

    currency translator

---
+++ CurrencyTranslator/Library.cs
@@ -0,0 +1,117 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace CurrencyTranslator
+{
+    public class Library
+    {
+        public static string Base_Currency = null;
+        public static string Translate_Currency = null;
+        public static string Ammount = null;
+
+        public static WebClient webClient = new WebClient();
+
+        public static async Task GetCurrencys()
+        {
+            try
+            {
+                Base_Currency = null;
+                Translate_Currency = null;
+                Ammount = null;
+
+                string translation_json = null;
+                dynamic translation_data = null;
+                translation_json = webClient.DownloadString($"https://api.frankfurter.app/currencies");
+                translation_data = JObject.Parse(translation_json);
+                Console.WriteLine(translation_data);
+
+                Console.ForegroundColor= ConsoleColor.Yellow;
+                Console.WriteLine("\n\n Press any Key to Continue!");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadLine(); Console.Clear(); return;
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine($"An Error Occured:   {ex.Message}");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        public static async Task GetTranslation()
+        {
+            try
+            {
+                Base_Currency = null;
+                Translate_Currency = null;
+                Ammount = null;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("State youre own Currency: (EUR/USD,etc)\n");
+                Console.ForegroundColor = ConsoleColor.White;
+                Base_Currency = Console.ReadLine(); Console.Clear();
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("State the Currency you wanna convert to:  (EUR/USD,etc)\n");
+                Console.ForegroundColor = ConsoleColor.White;
+                Translate_Currency = Console.ReadLine(); Console.Clear();
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("State the Amount you wann convert from youre own Currency to the other Currency:");
+                Console.ForegroundColor = ConsoleColor.White;
+                Ammount = Console.ReadLine(); Console.Clear();
+
+                string translation_json = null;
+                dynamic translation_data = null;
+                translation_json = webClient.DownloadString($"https://api.frankfurter.app/latest?amount={Ammount}&from={Base_Currency}&to={Translate_Currency}");
+                translation_data = JObject.Parse(translation_json);
+                var data = translation_data.rates;
+                Console.WriteLine(data);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\n\n Press any Key to Continue!");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadLine(); Console.Clear(); return;
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine($"An Error Occured:   {ex.Message}");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        public static async Task GetFromCurrency()
+        {
+            try
+            {
+                Base_Currency = null;
+                Translate_Currency = null;
+                Ammount = null;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("State the Currency you wanna search for: (EUR/USD,etc)\n");
+                Console.ForegroundColor = ConsoleColor.White;
+                Base_Currency = Console.ReadLine(); Console.Clear();
+
+                string translation_json = null;
+                dynamic translation_data = null;
+                translation_json = webClient.DownloadString($"https://api.frankfurter.app/latest?from={Base_Currency}");
+                translation_data = JObject.Parse(translation_json);
+                var data = translation_data.rates;
+                Console.WriteLine(data);
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\n\n Press any Key to Continue!");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadLine(); Console.Clear(); return;
+            }
+            catch (Exception ex )
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine($"An Error Occured:   {ex.Message}");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+    }
+}
---
+++ CurrencyTranslator/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CurrencyTranslator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            State();
+        }
+
+        public static void State()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Currency Translator\n\n 1. Show all Currencys\n 2. Make a Conversion\n 3. Get All from Currency ");
+            Console.ForegroundColor = ConsoleColor.White;
+            var answer = Console.ReadLine();
+            switch (answer)
+            {
+                case "1":
+                Console.Clear(); var task1 = Task.Run((Func<Task>)Library.GetCurrencys); task1.Wait(); break;
+                case "2":
+                Console.Clear(); var task2 = Task.Run((Func<Task>)Library.GetTranslation); task2.Wait(); break;
+                case "3":
+                Console.Clear(); var task3 = Task.Run((Func<Task>)Library.GetFromCurrency); task3.Wait(); break;
+            }
+            State();
+        }
+    }
+}
